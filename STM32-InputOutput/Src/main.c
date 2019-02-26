6c04ecda0574ee96af7d0c1b9ba6c6acf241ae1c
Prajuk Nusbaum <deomax@yahoo.com>
2019-02-25 21:05:28 -0600

    Changed PA0 to a GPIO Input

--- STM32-InputOutput/Src/main.c
+++ STM32-InputOutput/Src/main.c
@@ -174,11 +174,11 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
+  /*Configure GPIO pin : userButton_Pin */
+  GPIO_InitStruct.Pin = userButton_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(userButton_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD4_Pin LD3_Pin */
   GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
